490d5603addf2dc8cfc67db29feaf37fec579735
Alexander Vale <15g16483@marling.gloucs.sch.uk>
2020-09-18 10:41:18 +0000

    is odd/even exercise

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-let n = 2.34345
-let rounded = (Math.round(n*1000))/1000
-console.log(rounded)
+for(let i = 0; i<=20;i++){
+  if(i%2 === 0){
+    console.log(i, 'is even')
+  }else{
+    console.log(i, 'is odd')
+  }
+}
